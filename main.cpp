cdc04eff6d57e5083b7439419df968ef79c2a230
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-03 15:04:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include "Bitmap.h"
 #include <bitset>
-int main () {
-    Bitmap bmp ("C://Users//Mauricio//Desktop//Imagenes//po.bmp",2);
+int main (int argc, char** argv) {
+    bool cerrar = true;
+    int opcion = 0;
+    cout << "You have entered " << argc
+        << " arguments:" << "\n";
+
+    for (int i = 0; i < argc; ++i)
+        cout << argv[i] << "\n";
+
+    do {
+        cin >> opcion;
+        if (opcion==1) {
+            Bitmap bmp ("C://Users//Mauricio//Desktop//Imagenes//cuadro.bmp", 1);
+        } else if (opcion == 2) {
+            Bitmap bmp ("C://Users//Mauricio//Desktop//Imagenes//cuadro.bmp", 2);
+
+        } else if (opcion == -1) {
+            break;
+        }
+    } while (cerrar);
+   // cout<<bmp.getTexto ();
     return 0;
 }
