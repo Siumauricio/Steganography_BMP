040f20420f91fd58b8b063cdeaeb8af5a3903739
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-03 16:59:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include "Bitmap.h"
 #include <bitset>
-int main (int argc, char** argv) {
-    bool cerrar = true;
-    int opcion = 0;
-    cout << "You have entered " << argc
-        << " arguments:" << "\n";
-
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
-
-    do {
-        cin >> opcion;
-        if (opcion==1) {
-            Bitmap bmp ("C://Users//Mauricio//Desktop//Imagenes//cuadro.bmp", 1);
-        } else if (opcion == 2) {
-            Bitmap bmp ("C://Users//Mauricio//Desktop//Imagenes//cuadro.bmp", 2);
-
-        } else if (opcion == -1) {
-            break;
+#include <string>
+int main (int argc,char ** argv) {
+    string palabra;
+    for (int i = 0; i < argc; ++i){
+        std::string word = argv[i];
+        if(word=="-o"){
+            for (int j = i+2; j < argc; ++j) {
+                palabra+=argv[j];
+                palabra+=" ";
+            }
+            Bitmap bmp (argv[2],palabra, 1);
+        }else if(word=="-e"){
+            Bitmap bmp (argv[2],palabra, 2);
         }
-    } while (cerrar);
-   // cout<<bmp.getTexto ();
+}
     return 0;
 }
