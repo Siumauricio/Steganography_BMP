d25fcbb53245e0dc109f2e394cd79c853324af35
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-06 23:45:32 -0600

    Update Bitmap.h

--- Bitmap.h
+++ Bitmap.h
@@ -39,17 +39,19 @@ struct RGB24B {
 };
 class Bitmap {
 public:
+    int pos;
     BMP_Header Header;
     BMP_InfoHeader InfoHeader;
     Bitmap ();
-    Bitmap (string,string,int);
+    Bitmap (string,string,int,int);
     void ObtenerBmp_Header (string);
-    void ObtenerBmp_InfoHeader (string,string,int);
-
+    void ObtenerBmp_InfoHeader (string,string,int,int);
+    void EscribirEnArchivo(string);
     vector<RGB24B>Colores24;
     vector<char>Cadena;
     int posCadena = 0;
     int posTexto = 0;
+    string txtArchivo;
     string decimaltoBinary (int);
     vector<string> stringtoBinary (string);
     void Swap_RGB_String_R (string&, string);
@@ -57,12 +59,12 @@ public:
 
     void Swap_RGB_String_B (string&, string);
     uint8_t getDecimalFromBinary (string);
-
+    double cantidadPalabras();
     void getLastIndex (string);
     string getTextFromBinary (string);
     void EncryptMessage (string, string);
     void DecryptMessage ();
-    string getTexto ();
+    string getTexto (string);
 
     int getTipo ();
 };
