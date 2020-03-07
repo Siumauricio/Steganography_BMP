938d0fa6c7c95c779de96f6ab41dec6efccbe612
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-06 23:45:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,38 @@
 #include <string>
 int main (int argc,char ** argv) {
     string palabra;
-    for (int i = 0; i < argc; ++i){
-        std::string word = argv[i];
-        if(word=="-o"){
-            for (int j = i+2; j < argc; ++j) {
-                palabra+=argv[j];
-                palabra+=" ";
+    if(argc>=3){
+        string word = argv[1];
+        if (word == "-o"&& argc>=5) {
+            std::string word2 = argv[3];
+            if (word2 == "-m") {
+                for (int j = 4; j < argc; ++j) {
+                    palabra += argv[j];
+                    palabra += " ";
+                }
+                Bitmap bmp(argv[2], palabra, 1, 1);
+            } else if (word2 == "-f") {
+                Bitmap bmp(argv[2], argv[4], 1, 2);
             }
-            Bitmap bmp (argv[2],palabra, 1);
-        }else if(word=="-e"){
-            Bitmap bmp (argv[2],palabra, 2);
+
         }
-}
+        if(word=="-e"){
+            if(argc==5){
+                string word2 = argv[3];
+                if(word2=="-f"){
+                    Bitmap bmp(argv[2], argv[4], 2, 2);
+                }
+            }else if(argc==3){
+                Bitmap bmp(argv[2], palabra, 2, 1);
+            }
+        }
+
+        if(word=="-t"&&argc==3){
+            Bitmap bmp(argv[2], "", 3, 1);
+        }
+        if(word=="-v"&& argc==3){
+            Bitmap bmp(argv[2], "", 4, 1);
+        }
+    }
     return 0;
 }
